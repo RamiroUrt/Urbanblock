5c0d193a0da95a13fb4817873044f5cae713486d
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-03-25 11:19:22 -0300

    Update Menu.jsx

--- src/components/FlowingMenu/Menu.jsx
+++ src/components/FlowingMenu/Menu.jsx
@@ -1,13 +1,15 @@
 import FlowingMenu from './FLowingMenu';
 
+
 const demoItems = [
-    { link: '#', text: 'Innovación', image: '../public/img/menu/hero.webp' },
-    { link: '#', text: 'Desarrollo', image: '../public/img/menu/Desarrollo.webp' },
-    { link: '#', text: 'Progreso', image: '../public/img/menu/Progreso.webp' },
-    { link: '#', text: 'Excelencia', image: '../public/img/menu/4.webp' },
-    { link: '#', text: 'Transformación', image: '../public/img/menu/Excavator.webp' },
+    { link: '#', text: 'Innovación', image: './public/img/menu/hero.webp' },
+    { link: '#', text: 'Desarrollo', image: './public/img/menu/Desarrollo.webp' },
+    { link: '#', text: 'Progreso', image: './public/img/menu/Progreso.webp' },
+    { link: '#', text: 'Excelencia', image: './public/img/menu/4.webp' },
+    { link: '#', text: 'Transformación', image: './public/img/menu/Excavator.webp' },
 
 ];
+
 
 const Menu = () => {
   return (
